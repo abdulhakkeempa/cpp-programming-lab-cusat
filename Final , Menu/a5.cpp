7c82e05ebf69d1e12b993b0e2ac484bdaf5c2888
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2022-03-09 22:45:09 +0530

    Update a5.cpp

--- Final , Menu/a5.cpp
+++ Final , Menu/a5.cpp
@@ -2,17 +2,52 @@
 using namespace std;
 
 class A{
+	int **p;
+	int d1,d2;
 public:
-	A(){
-		cout<<"This is Constructor"<<endl;
+	A(int x,int y){
+		cout<<"Array dynamically allocated by constructor"<<endl;
+		d1 = x;
+		d2 = y;
+		p = new int *[d1];
+		for (int i = 0; i < d1; ++i) {
+			p[i] = new int [d2];
+		}
 	}
+	void get_data();
+	void put_data();
 	~A(){
-		cout<<"This is Destructor"<<endl;
+		cout<<"Memory released using destructor"<<endl;
 	}
 };
 
+void A::get_data(){
+	cout<<"Enter the Values Row by Row"<<endl;
+	for (int i = 0; i < d1; ++i) {
+		cout<<"Row - "<<i+1<<endl;
+		for (int j = 0; j < d2; ++j) {
+			cin>>p[i][j];
+		}
+	}
+}
+
+void A::put_data(){
+	for (int i = 0; i < d1; ++i) {
+		for (int j = 0; j < d2; ++j) {
+			cout<<p[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+}
+
+
 
 int main() {
-	A Object;
+	int row,col;
+	cout<<"Enter the rows and coloumns of the matrix"<<endl;
+	cin>>row>>col;
+	A Object(row,col);
+	Object.get_data();
+	Object.put_data();
 	return 0;
 }
