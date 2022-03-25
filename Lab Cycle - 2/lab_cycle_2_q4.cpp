cdebb1dbd3b60c2eacff93d32dcb278e04a9f643
Abdul Hakkeem P A <abdulhakkeempa2002@gmail.com>
2022-03-25 22:05:39 +0530

    Update lab_cycle_2_q4.cpp

--- Lab Cycle - 2/lab_cycle_2_q4.cpp
+++ Lab Cycle - 2/lab_cycle_2_q4.cpp
@@ -10,6 +10,15 @@ class Matrix{
 	int rows,coloumns;
 	int **matrix;
 public:
+	Matrix(){}
+	Matrix(int r,int c){
+		rows=r;
+		coloumns=c;
+		matrix = new int *[rows];
+		for (int i = 0; i < rows; ++i) {
+			matrix[i] = new int [coloumns];
+		}
+	}
 	//function to get the no of rows and coloumns to dynamically allocate memory
 	void getRowsandColoumns(){
 		cout<<"Enter the no of rows and coloumns"<<endl;
@@ -18,6 +27,8 @@ public:
 	void allocateMatrix();
 	void getMatrix();
 	void displayMatrix();
+	void operator+(const Matrix&);
+	void operator*(const Matrix&);
 	//destructor used to delete the dynamically allocated memory
 	~Matrix(){
 		for (int i = 0; i < rows; ++i) {
@@ -46,10 +57,9 @@ void Matrix::getMatrix(){
 		}
 	}
 }
-
 //function to display the matrix entered.
 void Matrix::displayMatrix(){
-	cout<<"The Matrix you have entered is "<<rows<<"x"<<coloumns<<endl;
+	cout<<"The Matrix is of dimension "<<rows<<"x"<<coloumns<<endl;
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < coloumns; ++j) {
 			cout<<matrix[i][j]<<"\t";
@@ -57,12 +67,84 @@ void Matrix::displayMatrix(){
 		cout<<"\n";
 	}
 }
-
+//overloading + operator for matrix addition
+void Matrix::operator+(const Matrix&a){
+	if(rows==a.rows and coloumns==a.coloumns){
+		Matrix sum(rows,coloumns);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < coloumns; ++j) {
+				sum.matrix[i][j]=matrix[i][j]+a.matrix[i][j];
+			}
+		}
+		sum.displayMatrix();
+	}
+	else{
+		cout<<"Addition not possible"<<endl;
+	}
+}
+//overloading * operator for matrix addition
+void Matrix::operator *(const Matrix&a){
+	if (coloumns==a.rows) {
+		Matrix productMatrix(rows,a.coloumns);
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < a.coloumns; ++j) {
+				int sum=0;
+				for (int k = 0; k < coloumns; ++k) {
+					sum = matrix[i][k]*a.matrix[k][j]+sum;
+					productMatrix.matrix[i][j] = sum;
+				}
+			}
+		}
+		productMatrix.displayMatrix();
+	} else {
+		cout<<"Multiplication not possible"<<endl;
+	}
+}
 int main() {
-	Matrix matrix1;
-	matrix1.getRowsandColoumns();
-	matrix1.allocateMatrix();
-	matrix1.getMatrix();
-	matrix1.displayMatrix();
+	int quitOption,exitLoop;
+	cout<<"Welcome\n1.Start\n2.Quit"<<endl;
+	cin>>quitOption;
+	if (quitOption==1) {
+		Matrix matrix1;
+		cout<<"Matrix - 1"<<endl;
+		matrix1.getRowsandColoumns();
+		matrix1.allocateMatrix();
+		matrix1.getMatrix();
+
+		Matrix matrix2;
+		cout<<"Matrix - 2"<<endl;
+		matrix2.getRowsandColoumns();
+		matrix2.allocateMatrix();
+		matrix2.getMatrix();
+
+		cout<<endl;
+		cout<<"Matrix - 1"<<endl;
+		matrix1.displayMatrix();
+		cout<<endl;
+		cout<<"Matrix - 2"<<endl;
+		matrix2.displayMatrix();
+
+		do {
+			int option;
+			cout<<"\n1.Matrix 1 + Matrix 2\n2.Matrix 1 x Matrix2"<<endl;
+			cin>>option;
+			switch (option) {
+				case 1:
+					matrix1+matrix2;
+					break;
+				case 2:
+					matrix1*matrix2;
+					break;
+				default:
+					break;
+			}
+			cout<<"\n1.Continue\n2.Quit"<<endl;
+			cin>>exitLoop;
+		} while (exitLoop==1);
+
+	} else {
+		cout<<"Successfully Quitted"<<endl;
+		return 0;
+	}
 	return 0;
 }
